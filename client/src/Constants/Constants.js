3c6f7817a65cff0813de3935fbfaad6a25bd3c4d
samaksh mittal <sam.mittal44@gmail.com>
2023-04-26 16:41:10 +0530

    chore: add colors and emailFetchUrl in constants

---
+++ client/src/Constants/Constants.js
@@ -0,0 +1,11 @@
+export const FETCH_EMAIL_BASE_URL = "https://flipkart-email-mock.vercel.app";
+
+export const colors = {
+  accent: "#e54065",
+  background: "#f4f5f9",
+  border: "#cfd2dc",
+  text: "#636363",
+  filterButton: "#e1e4ea",
+  readBackground: "#f2f2f2",
+  white: "#ffffff",
+};
